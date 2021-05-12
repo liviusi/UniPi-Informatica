c4f5df32459bf516907bc4b645f4e527342d61aa
Giacomo Trapani <giacomo.trapani01@libero.it>
2021-05-12 22:18:50 +0200

    Iniziato ad implementare readFile.

--- SOL/Laboratorio/Progetto/src/includes/server_api/server_api.c
+++ SOL/Laboratorio/Progetto/src/includes/server_api/server_api.c
@@ -9,17 +9,18 @@
 #include <fcntl.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <file_data.h>
+#include <filedata.h>
 #include <hashtable.h>
 
 #define BUCKETS 512
 #define UNIX_PATH_MAX 108
 
 static int client_fd;
-static struct hashtable_t* hashtable = NULL;
+static hashtable_t* hashtable = NULL;
 
 int openConnection(const char* sockname, int msec, const struct timespec abstime)
 {
+	if (hashtable == NULL) hashtable = HASHTABLE_INITIALIZER(BUCKETS);
 	struct sockaddr_un socket_address;
 	strncpy(socket_address.sun_path, sockname, UNIX_PATH_MAX);
 
@@ -51,16 +52,20 @@ int closeConnection(const char* sockname)
 
 int openFile(const char* pathname, int flags)
 {
-	mode_t oldmask = umask(033);
 	int open_flags = 0;
 	int* lockedby = NULL;
 	if (flags & O_CREATE) open_flags |= O_CREAT;
+	mode_t oldmask = umask(033);
 	int fd = open(pathname, open_flags);
+	umask(oldmask);
 	if (fd == -1) return -1;
 	if (flags & O_LOCK) lockedby = &client_fd;
-	umask(oldmask);
-	struct filedata* file = FILEDATA_INIT(fd, pathname, lockedby);
-	if (hashtable == NULL) hashtable = HASHTABLE_INITIALIZER(BUCKETS);
+	filedata_t* file = FILEDATA_INIT(fd, pathname, lockedby);
+	if (filedataAddOpenedBy(file, client_fd) == -1)
+	{
+		close(fd);
+		return -1;
+	}
 	hashtableInsert(hashtable, (void*) pathname, sizeof(pathname), (void*) file, sizeof(file));
 	free(file);
 	return 0;
@@ -72,11 +77,19 @@ int readFile(const char* pathname, void** buf, size_t* size)
 	if (tmp == NULL)
 	{
 		// should set errno to something
-		buf = NULL;
-		size = NULL;
+		*buf = NULL;
+		*size = NULL;
 		return -1;
 	}
-	struct filedata* file = (struct filedata*) tmp;
+	filedata_t* file = (filedata_t*) tmp;
+	if (!filedataIsOpenedBy(file, client_fd))
+	{
+		// file has yet to be opened.
+		free(file);
+		*buf = NULL;
+		*size = NULL;
+		return -1;
+	}
 	size_t sz = 0;
 	void* contents = fileGetContents(file, &sz);
 	*buf = contents;
@@ -87,6 +100,7 @@ int readFile(const char* pathname, void** buf, size_t* size)
 
 int writeFile(const char* pathname, const char* dirname)
 {
+
 	return 0;
 }
 
