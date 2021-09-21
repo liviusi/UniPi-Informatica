8ea9e38585a237a27dadd4c4e2d4259f6300015a
liviusi <giacomo.trapani01@libero.it>
2021-09-21 17:10:33 +0200

    Risolto esercizio 2.3

--- RCL/Lezione2/Esercizio3.java
+++ RCL/Lezione2/Esercizio3.java
@@ -6,7 +6,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class Esercizio3
 {
